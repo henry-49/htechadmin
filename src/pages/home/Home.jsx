d6eb6147b61d165137048e68ce950439ffa03446
henry-49 <henryswebdevelopment@gmail.com>
2022-04-29 21:27:57 +0200

    added widget and custom styles to the types

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Widget from '../../components/widget/Widget';
 
 import "./home.scss";
 
@@ -11,7 +12,12 @@ const Home = ()=> {
                <Sidebar />
                <div className="homeContainer">
                    <Navbar />
-                   <h2>Home Container</h2>
+                   <div className="widgets">
+                       <Widget type="user"/>
+                       <Widget type="order"/>
+                       <Widget type="earning"/>
+                       <Widget type="balance"/>
+                   </div>
                </div>
             </div>
         );
